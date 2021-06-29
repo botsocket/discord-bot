0afd6a42dc4b0bec2c56fc8f4a06deade24f511b
sammydalalli <86686585+sammydalalli@users.noreply.github.com>
2021-06-29 16:24:33 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    token: process.env.TOKEN,
+    token: process.env.TOKEN 'ODU5NTIxODI0MzM2NTEwOTc3.YNt6FA.TYtRI58CGaejvGIeuMUlUGTT1VM',
     app: '742291837674455090',
     guild: '736170183118290944',
 };
